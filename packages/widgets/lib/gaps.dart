61b88c6bc910df7de5d92b7d282201e40ac185df
Amit Kotlovski <amit@amitkot.com>
2022-12-21 09:19:30 +0200

    feat: initial commit

---
+++ packages/widgets/lib/gaps.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/widgets.dart';
+
+class Sizes {
+  static const double p4 = 4.0;
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p16 = 16.0;
+  static const double p20 = 20.0;
+  static const double p24 = 24.0;
+  static const double p28 = 28.0;
+  static const double p32 = 32.0;
+  static const double p36 = 36.0;
+  static const double p40 = 40.0;
+  static const double p44 = 44.0;
+  static const double p48 = 48.0;
+  static const double p52 = 52.0;
+  static const double p56 = 56.0;
+  static const double p60 = 60.0;
+  static const double p64 = 64.0;
+}
+
+const gapW4 = SizedBox(width: Sizes.p4);
+const gapW8 = SizedBox(width: Sizes.p8);
+const gapW12 = SizedBox(width: Sizes.p12);
+const gapW16 = SizedBox(width: Sizes.p16);
+const gapW20 = SizedBox(width: Sizes.p20);
+const gapW24 = SizedBox(width: Sizes.p24);
+const gapW28 = SizedBox(width: Sizes.p28);
+const gapW32 = SizedBox(width: Sizes.p32);
+const gapW36 = SizedBox(width: Sizes.p36);
+const gapW40 = SizedBox(width: Sizes.p40);
+const gapW44 = SizedBox(width: Sizes.p44);
+const gapW48 = SizedBox(width: Sizes.p48);
+const gapW52 = SizedBox(width: Sizes.p52);
+const gapW56 = SizedBox(width: Sizes.p56);
+const gapW60 = SizedBox(width: Sizes.p60);
+const gapW64 = SizedBox(width: Sizes.p64);
+
+const gapH4 = SizedBox(height: Sizes.p4);
+const gapH8 = SizedBox(height: Sizes.p8);
+const gapH12 = SizedBox(height: Sizes.p12);
+const gapH16 = SizedBox(height: Sizes.p16);
+const gapH20 = SizedBox(height: Sizes.p20);
+const gapH24 = SizedBox(height: Sizes.p24);
+const gapH28 = SizedBox(height: Sizes.p28);
+const gapH32 = SizedBox(height: Sizes.p32);
+const gapH36 = SizedBox(height: Sizes.p36);
+const gapH40 = SizedBox(height: Sizes.p40);
+const gapH44 = SizedBox(height: Sizes.p44);
+const gapH48 = SizedBox(height: Sizes.p48);
+const gapH52 = SizedBox(height: Sizes.p52);
+const gapH56 = SizedBox(height: Sizes.p56);
+const gapH60 = SizedBox(height: Sizes.p60);
+const gapH64 = SizedBox(height: Sizes.p64);
